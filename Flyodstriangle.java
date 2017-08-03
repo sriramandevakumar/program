73c443b18038858edaca60e6f88c8d958d814ffe
sriramandevakumar <sriramandevakumar@gmail.com>
2017-08-03 23:59:28 +0530

    Assignement done

---
+++ Flyodstriangle.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class Flyodstriangle {
+	
+	public static void main (String args[]){
+		int z=1;
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		for(int i=1;i<=n;i++){
+			for(int j=1;j<=i;j++){
+					if(i==n){
+						System.out.println(z+"");
+					}
+					z++;
+			}
+		}
+		
+		
+	}
+
+}
